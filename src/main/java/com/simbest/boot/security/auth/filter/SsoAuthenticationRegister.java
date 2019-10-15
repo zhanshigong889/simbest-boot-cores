c753951c45b96518337b1050a50c90026b4f7c54
Kyne <lishuyi@simbest.com.cn>
2019-10-15 11:04:08 +0800

    在线预览文件，支持任意免认证的URL

--- src/main/java/com/simbest/boot/security/auth/filter/SsoAuthenticationRegister.java
+++ src/main/java/com/simbest/boot/security/auth/filter/SsoAuthenticationRegister.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
-import java.util.Collection;
 import java.util.Map;
 import java.util.SortedSet;
 
