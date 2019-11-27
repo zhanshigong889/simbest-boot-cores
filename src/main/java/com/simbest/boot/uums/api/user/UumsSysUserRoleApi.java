06d311c54178f98f0a9ccadf10632c171bd1e412
lm8580552 <33541086+lm8580552@users.noreply.github.com>
2019-11-27 11:30:14 +0800

    新增用户角色api，增加修改某个角色下人员的方法

---
+++ src/main/java/com/simbest/boot/uums/api/user/UumsSysUserRoleApi.java
@@ -0,0 +1,67 @@
+/*
+ * 版权所有 © 北京晟壁科技有限公司 2008-2027。保留一切权利!
+ */
+
+package com.simbest.boot.uums.api.user;
+
+import com.mzlion.easyokhttp.HttpClient;
+import com.simbest.boot.base.web.response.JsonResponse;
+import com.simbest.boot.config.AppConfig;
+import com.simbest.boot.util.encrypt.RsaEncryptor;
+import com.simbest.boot.util.json.JacksonUtils;
+import com.simbest.boot.util.security.SecurityUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * <strong>Title : SysAppController</strong><br>
+ * <strong>Description : </strong><br>
+ * <strong>Create on : 2018/5/26/026</strong><br>
+ * <strong>Modify on : 2018/5/26/026</strong><br>
+ * <strong>Copyright (C) Ltd.</strong><br>
+ *
+ * @author LM liumeng@simbest.com.cn
+ * @version <strong>V1.0.0</strong><br>
+ *          <strong>修改历史:</strong><br>
+ *          修改人 修改日期 修改描述<br>
+ *          -------------------------------------------<br>
+ */
+
+@Component
+@Slf4j
+public class UumsSysUserRoleApi {
+    private static final String USER_MAPPING = "/action/user/role/";
+    private static final String SSO = "/sso";
+    @Autowired
+    private AppConfig config;
+    //private String uumsAddress="http://localhost:8080/uums";
+    @Autowired
+    private RsaEncryptor encryptor;
+
+    /**
+     * 更新应用下的角色里的人
+     * @param mapParam
+     * @param appcode
+     * @return
+     */
+    public int updateRoleUser( Map<String,Object> mapParam,String appcode){
+        String loginUser = SecurityUtils.getCurrentUserName();
+        log.debug("Http remote request user by username: {}", loginUser);
+        String json0=JacksonUtils.obj2json(mapParam);
+        String username1=encryptor.encrypt(loginUser);
+        String username2=username1.replace("+","%2B");
+        JsonResponse response= HttpClient.textBody(config.getUumsAddress() + USER_MAPPING + "updateRoleUser"+SSO+"?loginuser="+username2+"&appcode="+appcode)
+                .json( json0 )
+                .asBean(JsonResponse.class );
+        if(response==null){
+            log.error("--response对象为空!--");
+            return 0;
+        }
+        return (int)response.getData();
+    }
+
+}
+
