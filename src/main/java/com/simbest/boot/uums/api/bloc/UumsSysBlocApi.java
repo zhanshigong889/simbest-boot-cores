0a4d596b519e819151fd06690e44565835baa587
kyne0116 <kyne.li@hotmail.com>
2020-04-27 09:39:39 +0800

    修改集团查询参数

--- src/main/java/com/simbest/boot/uums/api/bloc/UumsSysBlocApi.java
+++ src/main/java/com/simbest/boot/uums/api/bloc/UumsSysBlocApi.java
@@ -42,7 +42,7 @@ public class UumsSysBlocApi {
         JsonResponse response= HttpClient.post(config.getUumsAddress() + BLOC_MAPPING + "findById"+SSO)
                 .param(AuthoritiesConstants.SSO_API_USERNAME, encryptor.encrypt(ApplicationConstants.ADMINISTRATOR))
                 .param(AuthoritiesConstants.SSO_API_APP_CODE,appcode)
-                .param("blocid", blocid)
+                .param("id", blocid)
                 .asBean(JsonResponse.class);
         if(response==null){
             log.error("--response对象为空!--");
