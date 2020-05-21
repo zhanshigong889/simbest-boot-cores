dd71e7099f525aadc61677bb367e978774851ac4
kyne0116 <kyne.li@hotmail.com>
2020-05-21 10:48:24 +0800

    GlobalErrorController不记录全局Web日志

--- src/main/java/com/simbest/boot/base/web/log/GlobalWebRequestLogger.java
+++ src/main/java/com/simbest/boot/base/web/log/GlobalWebRequestLogger.java
@@ -54,10 +54,10 @@ public class GlobalWebRequestLogger {
 
     private Set<String> notRecordController = ImmutableSet.of("com.simbest.boot.security.auth.controller.CaptchaController",
             "com.simbest.boot.security.auth.controller.LoginController", "com.simbest.boot.security.auth.controller.IndexController",
-            "com.simbest.boot.sys.web.SysHealthController");
+            "com.simbest.boot.sys.web.SysHealthController","com.simbest.exclude.web.GlobalErrorController");
 
-//    @Pointcut("execution(* *..web..*Controller.*(..))")
-    @Pointcut("execution(* *..*Controller.*(..))")
+    @Pointcut("execution(* *..web..*Controller.*(..))")
+//    @Pointcut("execution(* *..*Controller.*(..))")  会影响websocket下的controller，导致websocket无法使用
     public void webLog() { }
 
     /**
