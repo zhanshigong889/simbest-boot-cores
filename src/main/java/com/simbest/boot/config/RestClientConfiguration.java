9e8026e001d1e3692221e027e5c4446680dd036e
kyne0116 <kyne.li@hotmail.com>
2020-05-30 16:48:03 +0800

    解决RestTemplate 接口 HttpMessageConverter的文本支持

--- src/main/java/com/simbest/boot/config/RestClientConfiguration.java
+++ src/main/java/com/simbest/boot/config/RestClientConfiguration.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.FormHttpMessageConverter;
@@ -18,6 +19,7 @@ import org.springframework.http.converter.xml.SourceHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -48,10 +50,25 @@ public class RestClientConfiguration {
                 new ResourceHttpMessageConverter(),
                 new SourceHttpMessageConverter<>(),
                 new FormHttpMessageConverter(),
-                customJackson2HttpMessageConverter
+                customJackson2HttpMessageConverter,
+                new WxMappingJackson2HttpMessageConverter()
         );
         restTemplate.setMessageConverters(messageConverters);
         return restTemplate;
     }
 
+    /**
+     * 解决： org.springframework.web.client.RestClientException: Could not extract response: no suitable HttpMessageConverter found for response type [class com.simbest.boot.base.web.response.JsonResponse] and content type [text/javascript;charset=utf-8]
+     * 参考： https://blog.csdn.net/kinginblue/article/details/52706155
+     * 参考： https://blog.csdn.net/u011768325/article/details/77097655
+     */
+    public class WxMappingJackson2HttpMessageConverter extends MappingJackson2HttpMessageConverter {
+        public WxMappingJackson2HttpMessageConverter(){
+            List<MediaType> mediaTypes = new ArrayList<>();
+            mediaTypes.add(MediaType.TEXT_PLAIN); //加入text/plain类型的支持
+            mediaTypes.add(MediaType.TEXT_HTML);  //加入text/html类型的支持
+            mediaTypes.add (MediaType.parseMediaType ("text/javascript;charset=utf-8"));
+            setSupportedMediaTypes(mediaTypes);// tag6
+        }
+    }
 }
