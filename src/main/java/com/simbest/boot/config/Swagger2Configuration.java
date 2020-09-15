9b65a96d624c5f7f1ed57a852009362214ef796a
ljwwwz <lijianwu19890512@163.com>
2020-09-15 16:36:03 +0800

    删除依赖的包

--- src/main/java/com/simbest/boot/config/Swagger2Configuration.java
+++ src/main/java/com/simbest/boot/config/Swagger2Configuration.java
@@ -3,7 +3,6 @@
  */
 package com.simbest.boot.config;
 
-import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
