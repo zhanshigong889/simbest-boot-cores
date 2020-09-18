fb30f8bb88dede51247d4bfc748d21e69471ccd2
kyne0116 <kyne.li@hotmail.com>
2020-09-18 12:06:50 +0800

    升级更新退出登录处理器

--- src/main/java/com/simbest/boot/security/auth/handle/SuccessLogoutHandler.java
+++ src/main/java/com/simbest/boot/security/auth/handle/SuccessLogoutHandler.java
@@ -4,9 +4,12 @@
 package com.simbest.boot.security.auth.handle;
 
 import com.simbest.boot.constants.ApplicationConstants;
+import com.simbest.boot.util.ApplicationContextProvider;
+import com.simbest.boot.util.SpringContextUtil;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.security.core.Authentication;
@@ -34,6 +37,9 @@ public class SuccessLogoutHandler implements LogoutSuccessHandler {
     @Autowired
     private DefaultLogoutHandler defaultLogoutHandler;
 
+    @Autowired
+    private ApplicationContextProvider springContextUtil;
+
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
             throws IOException, ServletException {
@@ -41,15 +47,17 @@ public class SuccessLogoutHandler implements LogoutSuccessHandler {
         request.logout();
         defaultLogoutHandler.logout(request, response, authentication);
         response.setStatus(HttpServletResponse.SC_OK);
-        if(StringUtils.isNotEmpty(env.getProperty("spring.cas.cas-server-url-prefix"))){
-            log.warn("---------------------------------CAS LOGOUT START---------------------------------------------");
+
+        Object casSecurityConfigurer = null;
+        try{
+            casSecurityConfigurer = springContextUtil.getBean("casSecurityConfigurer");
+        }catch(NoSuchBeanDefinitionException e){
+        }
+        if(null != casSecurityConfigurer){
+            log.warn("---------------------------------CAS LOGOUT 重定向---------------------------------------------");
             response.sendRedirect(request.getContextPath() + ApplicationConstants.CAS_LOGOUT_PAGE);
-            log.warn("---------------------------------CAS LOGOUT END---------------------------------------------");
-        }
-        else if(StringUtils.isNotEmpty(env.getProperty("server.servlet.context-path"))) {
-            response.sendRedirect(env.getProperty("server.servlet.context-path") + ApplicationConstants.CAS_LOGOUT_PAGE);
-        }
-        else {
+            log.warn("---------------------------------CAS LOGOUT 重定向---------------------------------------------");
+        }else {
             response.setStatus(HttpServletResponse.SC_OK);
             request.getRequestDispatcher(ApplicationConstants.LOGIN_PAGE).forward(request, response);
         }
